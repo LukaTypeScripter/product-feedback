4a39013de737dd2e12cded380f76ce3e5f87e6c0
lshinjikashvili <lshinjikashvili@crocobet.com>
2023-10-10 11:22:46 +0400

    initial commit

---
+++ src/app/home-left/home-left.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-left',
+  templateUrl: './home-left.component.html',
+  styleUrls: ['./home-left.component.scss']
+})
+export class HomeComponent {
+
+}
