e6d15b4870b7208383a6fd9bac7c0175511afe99
lshinjikashvili <lshinjikashvili@crocobet.com>
2023-10-16 12:10:20 +0400

    feat:created reusable button

--- src/app/home-right/popup-sort/popup-sort.component.ts
+++ src/app/home-right/popup-sort/popup-sort.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductRequest } from 'src/models/data.model';
 
 @Component({
   selector: 'app-popup-sort',
   templateUrl: './popup-sort.component.html',
   styleUrls: ['./popup-sort.component.scss']
 })
-export class PopupSortComponent {
+export class PopupSortComponent implements OnInit {
+  @Input() filteredData: ProductRequest[][] = [];
+    ngOnInit(): void {
+      console.log("wada",this.filteredData);
+    }
 
 }
