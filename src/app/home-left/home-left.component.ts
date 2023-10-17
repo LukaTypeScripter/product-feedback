d53351a44c6c1e7fd2f0ad4ba3bbbfb5715ed2ba
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2023-10-17 20:42:26 +0400

    added service

--- src/app/home-left/home-left.component.ts
+++ src/app/home-left/home-left.component.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Data, ProductRequest } from 'src/models/data.model';
 import { data } from '../data';
+import { DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'home-left',
@@ -11,50 +12,28 @@ import { data } from '../data';
 })
 export class HomeComponent implements OnInit {
   categories: string[] = ["all", "UI", "UX", "enhancement", "feature", "Bug"];
-  private dataSubject = new BehaviorSubject<Data[]>([]);
-  private activeCategorySubject = new BehaviorSubject<string>('all');
-  private sidebarOpenSubject = new BehaviorSubject<boolean>(false);
 
-  // Observable properties
-  datas$ = this.dataSubject.asObservable();
-  activeCategory$ = this.activeCategorySubject.asObservable();
-  sidebarOpen$ = this.sidebarOpenSubject.asObservable();
-
-  // Computed observable for filteredData using combineLatest
-  filteredData$: Observable<ProductRequest[][]> = combineLatest([
-    this.datas$,
-    this.activeCategory$
-  ]).pipe(
-    map(([data, activeCategory]) => {
-      if (activeCategory.toLowerCase() === 'all') {
-        return data.map((item) => item.productRequests);
-      } else {
-        return data
-          .map((item) =>
-            item.productRequests.filter((request: ProductRequest) =>
-              request.category.toLowerCase() === activeCategory.toLowerCase()
-            )
-          );
-      }
-    })
-  );
-
-  constructor() {
-    this.dataSubject.next(data);
-    this.activeCategorySubject.next('all');
+  filteredData$!: Observable<ProductRequest[][]>;
+  sidebarOpen$!: Observable<boolean>;
+  activeCategory$!: Observable<string>;
+  constructor(private dataService: DataService) {
+    this.filteredData$ = this.dataService.getFilteredData$();
   }
 
   ngOnInit() {
+    this.filteredData$ = this.dataService.getFilteredData$();
     this.filteredData$.subscribe(filteredData => {
       console.log('Filtered Data:', filteredData);
     });
+    this.sidebarOpen$ = this.dataService.sidebarOpen$;
+    this.activeCategory$ = this.dataService.activeCategory$;
   }
 
-  filterItems(category: string): void {
-    this.activeCategorySubject.next(category);
-  }
+filterItems(category: string): void {
+  this.dataService.setActiveCategory(category);
+}
 
-  toggleSidebar(event: boolean) {
-    this.sidebarOpenSubject.next(event);
-  }
+toggleSidebar(event: boolean) {
+  this.dataService.toggleSidebar(event);
+}
 }
--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { Data } from 'src/models/data.model';
+import { BehaviorSubject, Observable, combineLatest, map, of } from 'rxjs';
+import { Data, ProductRequest } from 'src/models/data.model';
 import { data } from 'src/app/data';
 
 @Injectable({
@@ -9,8 +9,41 @@ import { data } from 'src/app/data';
 })
 export class DataService {
  
-  getData(): Observable<Data[]> {
-    return of(data);
+  private dataSubject = new BehaviorSubject<Data[]>([]);
+  private activeCategorySubject = new BehaviorSubject<string>('all');
+  private sidebarOpenSubject = new BehaviorSubject<boolean>(false);
+  datas$ = this.dataSubject.asObservable();
+  activeCategory$ = this.activeCategorySubject.asObservable();
+  sidebarOpen$ = this.sidebarOpenSubject.asObservable();
+  // Add other methods and observables as needed
+
+  constructor() {
+    this.dataSubject.next(data);
+    this.activeCategorySubject.next('all');
   }
 
+  // Move the combineLatest logic to a method in this service
+  getFilteredData$(): Observable<ProductRequest[][]> {
+    return combineLatest([this.datas$, this.activeCategory$]).pipe(
+      map(([data, activeCategory]) => {
+        if (activeCategory.toLowerCase() === 'all') {
+          return data.map((item) => item.productRequests);
+        } else {
+          return data
+            .map((item) =>
+              item.productRequests.filter((request: ProductRequest) =>
+                request.category.toLowerCase() === activeCategory.toLowerCase()
+              )
+            );
+        }
+      })
+    );
+  }
+  setActiveCategory(category: string): void {
+    this.activeCategorySubject.next(category);
+  }
+
+  toggleSidebar(event: boolean): void {
+    this.sidebarOpenSubject.next(event);
+  }
 }
